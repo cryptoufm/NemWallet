6196f51dc72b13e90e48fac7bee220c7080feaad
Luis Angel <tortoladev@gmail.com>
2018-08-29 02:07:41 -0600

    Dependencies added and saved | Basic Native-Base structure set

--- nemWallet/App.js
+++ nemWallet/App.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+    state = {
+      fontLoaded: false,
+    };
+  // constructor() {
+
+    async componentWillMount() {
+      await Expo.Font.loadAsync({
+        'Roboto': require('native-base/Fonts/Roboto.ttf'),
+        'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      });
+      this.setState({ fontLoaded: true });
+    } 
+  // }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text> Nem Walle </Text>
-        <Text> CryptoUFM  </Text>
-      </View>
+      this.state.fontLoaded ? (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Nem Wallet</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>
+            Welcome...
+          </Text>
+        </Content>
+        <Footer>
+          <Text>2018, CryptoUFM</Text>            
+        </Footer>
+      </Container> ) : null 
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#20b5ac',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#fff',
-  },
-});
